f0609305012de1a52ef2fb191c29e855bc61a757
Mahshid Zandian <zandian.mahshid@gmail.com>
2023-04-26 14:37:22 -0700

    Add my college project to Portfolio component

---
+++ src/components/portfolio/projects/Seasons.jsx
@@ -0,0 +1,13 @@
+import Iframe from 'react-iframe';
+
+export default function Seasons() {
+    return (
+        <Iframe src="/projects/seasons/FinalProject.html"
+            width="640px"
+            height="320px"
+            id=""
+            className=""
+            display="block"
+            position="relative"/>
+    );
+}
